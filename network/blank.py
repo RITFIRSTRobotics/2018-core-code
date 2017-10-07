a3a807cb7e5ecd48acaa6ed2c3c83a33e5aa997a
thatging3rkid <thatging3rkid@users.noreply.github.com>
2017-10-07 13:50:53 -0400

    added port

--- network/blank.py
+++
@@ -1,5 +0,0 @@
-# this is not an empty python file
-
-def test():
-    print("testing...")
-    print("more testing...")
---
+++ network/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants for networking
+
+:author: Connor Henley @thatging3rkid
+"""
+
+PORT = int(463)
