6f9dc4aa81b51c9661ec57789662d1446c5d21f9
thatging3rkid <thatging3rkid@users.noreply.github.com>
2017-10-08 22:19:25 -0400

    Started work on the Constants for the Arduino<->FMS connection

---
+++ serial/Constants.hpp
@@ -0,0 +1,12 @@
+/**
+ * Constants.hpp
+ *
+ * A bunch of constants used for serial transmission between the FMS and the scoring Arduino
+ */
+
+#define BAUD_RATE 250000 // Baud Rate of the connection
+
+/**
+ * A String sent to the Arduino when Serial has been initalized
+ */
+#define INIT_STRING "i0"
