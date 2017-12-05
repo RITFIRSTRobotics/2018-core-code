6c3fbcce55ea98409a6c3e341c624f53c846d856
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-05 01:39:15 -0500

    continued work on controller

--- i2c/Constants.hpp
+++ i2c/Constants.hpp
@@ -8,4 +8,11 @@
 #ifndef _i2c_constants_hpp
 #define _i2c_constants_hpp
 
+#define BASE_ADDR 0x10 // Base i2c address (addresses should add to this)
+
+// #defines for the the addresses
+#define ADDR0 BASE_ADDR + 1
+#define ADDR1 BASE_ADDR + 2
+#define ADDR2 BASE_ADDR + 3
+
 #endif
