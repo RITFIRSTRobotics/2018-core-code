76433fd743905e0027423141dc2cc7bd92179ddc
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-10 16:17:58 -0500

    added new #defines

--- i2c/i2c_constants.hpp
+++ i2c/i2c_constants.hpp
@@ -15,4 +15,13 @@
 #define ADDR1 BASE_ADDR + 2
 #define ADDR2 BASE_ADDR + 3
 
+// #defines for transmission data
+#define I2CDATA_DELIMITER ':'
+#define I2CDATA_BUFFER_LEN 80
+#define dlm I2CDATA_DELIMITER
+// formatting string, should evaluate to "d:%03d:...:%01d:..."
+#define I2CDATA_FORMAT_STRING "d" + dlm + "%03d" + dlm + "%03d" + dlm + "%03d" + dlm \
+	+ "%03d" + dlm + "%01d" + dlm + "%01d" + dlm + "%01d" + dlm + "%01d" + dlm
+#undef dlm
+
 #endif
