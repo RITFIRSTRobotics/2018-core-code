fca4530f5677e97ec69ee9fd4dfa4edc31040855
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-23 19:17:55 -0500

    changed around Packet so that it could be serialized

--- network/Packet.py
+++ network/Packet.py
@@ -1,21 +1,28 @@
 from enum import Enum
 
-import sys
 
+class StorageType(Enum):
+    """
+    Defines the type of data stored in a packet
+    """
+
+    REQUEST = 0, 'request'
+    RESPONSE = 1, 'response'
+    STATUS = 2, 'status'
+
+    def __new__(cls, value, name):
+        member = object.__new__(cls)
+        member._value_ = value
+        member.fullname = name
+        return member
+
+    def __int__(self):
+        return int(self.value)
 
 class Packet(object):
     """
     Contains data that is sent between the FMS and a robot
     """
-
-    class StorageType(Enum):
-        """
-        Defines the type of data stored in a packet
-        """
-
-        REQUEST = 0
-        RESPONSE = 1
-        STATUS = 2
 
     __slots__ = ["type", "data"]
 
