9924fb339b3e102a4abe8c4d86fb259687bcf66a
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-25 15:16:54 -0500

    added controller number to the transmissions

--- serial/usbser_constants.hpp
+++ serial/usbser_constants.hpp
@@ -24,8 +24,8 @@
 #define CALIBRATE_MESSAGE "c0:%1u" // calibrate goal %b
 #define CALIBRATE_RESPONSE "c1:good" // tell the FMS that the goal has been calibrated
 
-#define CONTROLLER_DATA "md:%u:%u:%u:%u:%u" // movement data of the two sticks (%u is an unsigned byte) 
-                                            //and a number representative of all the buttons being pressed
+#define CONTROLLER_DATA "md:%u:%u:%u:%u:%u:%u" // controller number, movement data of the two sticks (unsigned byte) 
+                                               // and a number representative of all the buttons being pressed
 
 #define SCORE_DATA "sd:%d" // data sent when a goal has been made (send just the goal number, FMS is responsible for the point value)
 
