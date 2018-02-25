d821ed5e21bc3ba543fb9c9adb7f7ec7c4191567
thatging3rkid <thatging3rkid@users.noreply.github.com>
2018-02-25 00:03:11 +0000

    changed FMS port (because less than 1024 requires root)

--- network/constants.py
+++ network/constants.py
@@ -4,8 +4,8 @@ Constants for networking
 :author: Connor Henley @thatging3rkid
 """
 
-PORT = int(463)
-LOG_PORT = int(464) # not currently used
+PORT = int(1463)
+LOG_PORT = int(1464) # not currently used
 
 FMS_IP = "10.0.1.2"
 
