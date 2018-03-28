3e92f737278eb5e585af5c9ce5be9f42c2fe2409
Connor Henley <thatging3rkid@users.noreply.github.com>
2018-03-28 13:57:34 -0400

    added util file with function to get IP

---
+++ network/utils.py
@@ -0,0 +1,19 @@
+"""
+Utility file for Networking
+
+:author: Connor Henley @thatging3rkid
+"""
+import socket
+
+# shamelessly stolen from https://stackoverflow.com/a/28950776
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
