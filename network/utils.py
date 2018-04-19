fe226a7090835b0cb98e8799f8b76adb53129cba
Connor Henley <thatging3rkid@users.noreply.github.com>
2018-04-19 11:39:09 -0400

    third time's a charm for get_ip?

--- network/utils.py
+++ network/utils.py
@@ -3,11 +3,15 @@ Utility file for Networking
 
 :author: Connor Henley @thatging3rkid
 """
+import fcntl
 import socket
+import struct
 
-# shamelessly stolen from https://stackoverflow.com/a/1267524
-def get_ip():
-    try:
-        return [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1][0]
-    except:
-        return "127.0.0.1"
+# shamelessly stolen from https://stackoverflow.com/a/24196955
+def get_ip(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
